6026fd1cc0c00f6a8fc4103a34af8ffa2f8074db
Dario Seminara <darios3@gmail.com>
2019-01-08 19:41:37 -0300

    generated build via 'yarn build'

---
+++ build/precache-manifest.f81c125d10070b41a7f1057f694e39b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "27af9f34bd3059e1c09f",
+    "url": "/static/js/main.27af9f34.chunk.js"
+  },
+  {
+    "revision": "837dc9500e97599bb433",
+    "url": "/static/js/1.837dc950.chunk.js"
+  },
+  {
+    "revision": "27af9f34bd3059e1c09f",
+    "url": "/static/css/main.402ef8bb.chunk.css"
+  },
+  {
+    "revision": "dcca9545b63ba5b34ec592f0a48995ad",
+    "url": "/index.html"
+  }
+];
